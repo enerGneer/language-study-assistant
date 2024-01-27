c3d98ba3caa6f0c2368a14bbbbdd6cc6419716f2
enerGneer <energneer@gmail.com>
2024-01-27 16:38:00 +0900

    chore: Rename project and introduce PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+import purgecss from "@fullhuman/postcss-purgecss";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    purgecss({
+      content: ["./**/*.html"],
+    }),
+  ],
 };
