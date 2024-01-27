42d5a5be3c0ef57c5c3fd7bb00a0d7408364939d
enerGneer <energneer@gmail.com>
2024-01-28 01:00:47 +0900

    refactor: Refactor CSS using PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,15 @@ import purgecss from "@fullhuman/postcss-purgecss";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-module.exports = {
+export default {
   plugins: [
     tailwindcss,
     autoprefixer,
     purgecss({
       content: ["./**/*.html"],
+
+      // 필요한 경우 CSS 클래스를 올바르게 추출하기 위한 기본 추출기 추가
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
     }),
   ],
 };
